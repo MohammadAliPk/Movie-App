c798030f568d5461a60a47b9bdc2b90279fe5bd8
Mohammad Ali <aliparkour83@gmail.com>
2023-12-18 04:52:07 +0330

    sidebar bugs fixed

--- src/Components/shared/Header.js
+++ src/Components/shared/Header.js
@@ -1,24 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { scaleRotate as Menu } from 'react-burger-menu';
+import { slide as Menu } from 'react-burger-menu';
 import './Header.css';
 
 const Header = () => {
 
+  const [isOpen, setOpen] = useState(false);
+
+  const handleIsOpen = () => {
+    setOpen(!isOpen);
+  };
+
+  const closeSideBar = () => {
+    setOpen(false);
+  };
+
+
   return (
-    <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} >
-      <nav className="bm-item-list">
-        <h1 id="logo" className="bm-item">Movie App</h1>
-        <Link to="/movies" className="bm-item">
-          Movies
-        </Link>
-        <Link to="/series" className="bm-item" >
-          Series
-        </Link>
-        <Link to="/trending" className="bm-item">
-          Trendings
-        </Link>
-      </nav>
+    <Menu isOpen={isOpen} onOpen={handleIsOpen} onClose={handleIsOpen}>
+      <h1 id="logo" className="bm-item">Movie App</h1>
+      <Link to="/movies" className="bm-item" onClick={closeSideBar}>
+        Movies
+      </Link>
+      <Link to="/series" className="bm-item" onClick={closeSideBar}>
+        Series
+      </Link>
+      <Link to="/trending" className="bm-item" onClick={closeSideBar}>
+        Trendings
+      </Link>
     </Menu>
   );
 };
